57ef83c6d960daa202c25f8b970838ea10bdd33f
aegisfleet <aegisfleet@gmail.com>
2024-04-23 22:31:58 +0900

    前回と違うデータを取得する対応

--- qiita_utils.py
+++ qiita_utils.py
@@ -2,7 +2,11 @@ import requests
 from xml.etree import ElementTree as ET
 from bs4 import BeautifulSoup
 
-def fetch_trending_articles(url="https://qiita.com/popular-items/feed", max_articles=5):
+import artifact_utils
+
+def fetch_trending_articles(url="https://qiita.com/popular-items/feed", count=5):
+    previous_articles = artifact_utils.load_previous_results()
+
     articles = []
     
     response = requests.get(url)
@@ -10,11 +14,15 @@ def fetch_trending_articles(url="https://qiita.com/popular-items/feed", max_arti
 
     namespaces = {'atom': 'http://www.w3.org/2005/Atom'}
 
-    for entry in root.findall('atom:entry', namespaces)[:max_articles]:
+    for entry in root.findall('atom:entry', namespaces)[:10]:
+        if len(articles) >= count:
+            break
         link = entry.find('atom:link[@rel="alternate"]', namespaces).attrib['href']
-        title = entry.find('atom:title', namespaces).text
-        articles.append((link, title))
+        if link not in previous_articles:
+            title = entry.find('atom:title', namespaces).text
+            articles.append((link, title))
 
+    artifact_utils.save_results(articles)
     return articles
 
 def fetch_article_content(url):
